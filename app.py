b5b2aaa33912ea053b9b6b34105b3df039b2c209
Ravinder1729 <136672445+Ravinder1729@users.noreply.github.com>
2024-07-01 20:03:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+import cv2
+
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+st.title("Face and Eyes Detection App")
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
+    st.image(img, channels="BGR")
+
+    if st.button("Detect"):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (127, 0, 255), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            
+            eyes = eye_classifier.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+        
+        st.image(img, channels="BGR")
+
